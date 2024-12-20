91691a36ab37e96843929a7b08008c39c11e25b3
José Antonio Llorens Llorens Padilla <jallorens@renr.es>
2024-12-20 13:40:44 +0100

    modulos

--- src/componentes/mapas/Mapa.jsx
+++ src/componentes/mapas/Mapa.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 
+const containerStyle = {
+    width: '400px',
+    height: '400px'
+};
+  
+const center = {
+    lat: -3.745,
+    lng: -38.523
+};
+
 export const Mapa = () => {
 
-    return(
-        <div>
-            
-        </div>
-    )
+    return (
+        <LoadScript googleMapsApiKey="TU_API_KEY">
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+          >
+            { /* Child components, such as markers, info windows, etc. */ }
+          </GoogleMap>
+        </LoadScript>
+      )
 
 
 }
